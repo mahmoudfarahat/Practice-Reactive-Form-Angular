5c7b645bad048a4474ce3a1f6f64534a4631e407
Mahmoud Farahat <Mahmoud.Farahat@corp.arab-computers.com>
2022-11-07 15:25:40 +0200

    1.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
   genders = ['male', 'female'];
 
   forbiddenName = ['ahmed','maro']
@@ -14,13 +18,19 @@ export class AppComponent implements OnInit {
 ngOnInit(): void {
   this.signupForm =  new FormGroup({
     'userData': new FormGroup({
-      'username' : new FormControl(null, [Validators.required,this.forbiddenNames.bind(this) ]),
-      'email' : new FormControl(null , [Validators.required , Validators.email])
+      'username' : new FormControl(null, [Validators.required,this.forbiddenNames.bind(this) ] ),
+      'email' : new FormControl(null , [Validators.required , Validators.email], this.forbiddenEmail)
     }),
 
     'gender': new FormControl('male'),
     'hobbies': new FormArray([])
   })
+
+  this.signupForm.get('userData.username').valueChanges.subscribe((value) =>
+  {
+     console.log(value)
+  }
+  )
 }
 onSubmit()
 {
@@ -43,6 +53,19 @@ if(this.forbiddenName.indexOf(control.value) !== -1 ){
   return {'nameIsForbbin':true}
 }
 return null
+}
+
+forbiddenEmail(control : FormControl) : Promise<any> | Observable<any> {
+  const promise = new Promise<any>((res,rej) => {
+    setTimeout(() => {
+      if(control.value == 'test@test.com'){
+        res({'emailIsForbidden': true})
+      }else{
+        res(null)
+      }
+    },1500)
+  })
+  return promise
 }
 
 
